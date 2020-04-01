b453dc6e15a615b01328015708abdf9c5b3adfb0
youssif-sully <ymsyahia@hotmail.com>
2020-04-01 03:03:19 +0200

    create csv_write

---
+++ csv_module/csv_module_2.py
@@ -0,0 +1,10 @@
+###############################################
+#                                             #
+#       Created by Youssef Sully              #
+#       Beginner 2 python                     #
+#       CSV module                            #
+#       Write CSV file                        #
+#                                             #
+###############################################
+
+import csv
